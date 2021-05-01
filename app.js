03820767414f188e4c625d812cef495693adc03b
Peter Quaranta <peter.quaranta@gmail.com>
2021-05-01 18:23:10 -0400

    Scanner APIs filled in, port from config instead of hardcoded

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8000;
 
 // Use dotenv for app config
 require('dotenv').config();
@@ -8,7 +9,7 @@ require('dotenv').config();
 require('./routes/management')(app);
 require('./routes/events')(app);
 
-// Start the app on port 3000
-app.listen(3000, () => {
-    console.log('App running on port 3000');
+// Start the app
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
--- routes/management.js
+++ routes/management.js
@@ -1,18 +1,43 @@
 // Management API for managing scanners and access rules
 module.exports = function(app) {
+    // Fake DB to store scanner information
+    const scannerDb = new Map();
+    // Auto increment used for generating unique IDs for the scanner
+    let scannerAutoInc = 0;
+
     // Endpoint for creating a new scanner
     app.post('/scanner', (req, res) => {
-        res.send('Scanner created');
+        // Generate the new scanner, add it to the DB, then return the json to the caller
+        const scanner = {
+            apiKey: `scanner-${scannerAutoInc}-api-key`,
+            id: `${scannerAutoInc++}`,
+            creationTimestamp: Date.now()
+        };
+
+        scannerDb.set(scanner.id, scanner);
+        console.log(`Scanner ${JSON.stringify(scanner)} created`);
+        res.json(scanner);
     });
 
     // Endpoint for retrieving all scanners in the system
     app.get('/scanners', (req, res) => {
-        res.send('Scanners retrieved');
+        // Return all values in the fake DB
+        res.json(Array.from(scannerDb.values()));
     });
 
     // Endpoint for deleting a scanner
-    app.delete('/scanner', (req, res) => {
-        res.send('Scanner deleted');
+    app.delete('/scanner/:id', (req, res) => {
+        let msg = ``;
+        if (!scannerDb.has(req.params.id)) {
+            msg = `Scanner "${req.params.id}" not found`;
+            res.status(404).send(msg);
+        } else {
+            // Delete the scanner from the fake DB
+            scannerDb.delete(req.params.id);
+            msg = `Scanner ${req.params.id} has been deleted`;
+            res.send(msg);
+        }
+        console.log(msg);
     });
 
     // Endpoint for creating a new rule
