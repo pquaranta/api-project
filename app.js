3bffc9436e539f3e2c15738e60cff2a05aceb0e3
Peter Quaranta <peter.quaranta@gmail.com>
2021-05-01 14:25:01 -0400

    Upgraded npm/node version which now includes package lock, other formatting changes included

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 // Import the separated routes
-require('./routes/management')(app)
-require('./routes/events')(app)
+require('./routes/management')(app);
+require('./routes/events')(app);
 
 // Start the app on port 3000
 app.listen(3000, () => {
